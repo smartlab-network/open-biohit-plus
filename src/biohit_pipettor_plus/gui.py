0fbd6ef644deeb26d52ebce2963aa01d482ab388
tiagomey <tiago.meyer.vallilengua@gmail.com>
2025-09-30 16:34:33 +0200

    gui start

--- src/biohit_pipettor_plus/gui.py
+++ src/biohit_pipettor_plus/gui.py
@@ -1,5 +1,69 @@
 import ttkbootstrap as ttk
+from .deck import Deck
+from .slot import Slot
 
 class Gui:
-    def __init__(self):
+    def __init__(self,deck: Deck, master: ttk.Window = None):
+        if isinstance(master, ttk.Window):
+            self.root = ttk.Toplevel(master)
+        else:
+            self.root = ttk.Window()
+
+        self.deck = deck
+        self.root.geometry("1400x800")
+
+        self.slots: dict[str, Slot] = deck.slots
+        self.slot_frames: dict[str, ttk.Frame] = {}
+
+        self.deck_canvas = ttk.Canvas(self.root,
+                                      width=abs(deck.range_x[0]-deck.range_x[1]),
+                                      height=abs(deck.range_y[0]-deck.range_y[1]),
+                                      background = "lightgray")
+
+        self.deck_canvas.create_rectangle(deck.range_x[0], deck.range_y[0], deck.range_x[1], deck.range_y[1], outline = "black", width=2)
+        self.deck_canvas.grid(row=0, column=0, sticky="nsew")
+        self.place_slots()
+        self.fill_slot_frames()
+
+    def get_root(self):
+        return self.root
+
+    def place_slots(self):
+        for slot_id, slot in self.slots.items():
+            frame = ttk.Frame(self.root,
+                              width=abs(slot.range_x[0]-slot.range_x[1]),
+                              height=abs(slot.range_y[0]-slot.range_y[1]))
+            frame.propagate = False
+            frame.columnconfigure(0, weight=1)
+            frame.rowconfigure(0, weight=1)
+            title = ttk.Label(frame,text=slot_id, anchor="center", font=("Arial", 18))
+            title.grid(row=0, column=0, sticky="nsew")
+            title.grid(row=0, column=0, sticky="nsew")
+
+            self.deck_canvas.create_window(slot.range_x[0], slot.range_y[0], window=frame, anchor="nw")
+            self.deck_canvas.create_rectangle(slot.range_x[0], slot.range_y[0], slot.range_x[1], slot.range_y[1], outline="red")
+
+            self.slot_frames[slot_id] = frame
+
+
+
+    def fill_slot_frames(self):
+        for slot_id, frame in self.slot_frames.items():
+            slot: Slot = self.slots[slot_id]
+            add_labware_button = ttk.Button(frame, command=lambda: self.callback_add_labware(slot_id), text="add labware")
+            add_labware_button.grid(row=1, column=0, sticky="nsew")
+            row = 1
+            for labware_id, labware_list in slot.labware_stack.items():
+                labware_label = ttk.Label(frame,
+                                          text=f"{labware_id}: {labware_list[1]}",
+                                          anchor="center",
+                                          font=("Arial", 18))
+                row += 1
+                labware_label.grid(row=row, column=0, sticky="nsew")
+
+
+    def slot_button_press(self, slot_id: str):
+        pass
+
+    def callback_add_labware(self, slot_id: str):
         pass
--- src/biohit_pipettor_plus/pipettor_plus.py
+++ src/biohit_pipettor_plus/pipettor_plus.py
@@ -1,8 +1,11 @@
 from biohit_pipettor import Pipettor
 from typing import Literal
 
+from .deck import Deck
+
+
 class PipettorPlus(Pipettor):
-    def __init__(self, tip_volume: Literal[200, 1000], *, multichannel: bool, initialize: bool = True):
+    def __init__(self, tip_volume: Literal[200, 1000], *, multichannel: bool, initialize: bool = True, deck: Deck):
         """
                 Interface to the Biohit Roboline pipettor
 
@@ -11,4 +14,45 @@ class PipettorPlus(Pipettor):
                 :param initialize: If True, the device will be initialized
                 """
         super().__init__(tip_volume=tip_volume, multichannel=multichannel, initialize=initialize)
+        self.deck = deck
+        self.slots: dict[str, Slot] = deck.slots
 
+
+    def fill_medium(self, plate_id: str):
+        pass
+
+    def remove_medium(self, plate_id: str):
+        pass
+
+    def discard_tips(self) -> None:
+        pass
+
+    def  pick_multi_tips(self) -> None:
+        pass
+
+    def replace_tips(self) -> None:
+        pass
+
+    def replace_multi(self):
+        pass
+
+    def  remove_multi(self):
+        pass
+
+    def fill_multi(self):
+        pass
+
+    def change_medium_multi(self):
+        pass
+
+    def dilute_multi(self):
+        pass
+
+    def spite_all(self):
+        pass
+
+    def spit(self):
+        pass
+
+    def home(self):
+        pass
