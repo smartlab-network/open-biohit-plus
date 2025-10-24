1cdcc8b635e57af008d56d6a899f951a0349a782
tiagomey <tiago.meyer.vallilengua@gmail.com>
2025-10-24 16:29:08 +0200

    test

--- src/tests/restore_from_json.py
+++ src/tests/restore_from_json.py
@@ -2,7 +2,7 @@ from src.biohit_pipettor_plus.control_json import read_json
 from src.biohit_pipettor_plus.deck import Deck
 from src.biohit_pipettor_plus.slot import Slot
 from src.biohit_pipettor_plus.labware import *
-from src.biohit_pipettor_plus.gui import Gui
+from src.biohit_pipettor_plus.gui_old import Gui
 deck1:Deck = read_json("deck_1")
 
 print(deck1)
--- src/tests/test_gui.py
+++ src/tests/test_gui.py
@@ -1,7 +1,8 @@
 from src.biohit_pipettor_plus.control_json import read_json
-from src.biohit_pipettor_plus.gui import Gui
+from src.biohit_pipettor_plus.gui_old import Gui
 restored_deck = read_json("deck1")
 
 gui = Gui(deck=restored_deck)
 root = gui.get_root()
 root.mainloop()
+
